b51c7e38b4367a14b755080a35f5e3593f2b839d
YoungHypo <haiboyang@smail.nju.edu.cn>
2025-02-25 23:18:43 -0800

    update web Icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ const poppins = Poppins({
 
 export const metadata: Metadata = {
   title: "HaiboYang Website",
-  description: "Personal portfolio and resume website",
+  description: "Personal portfolio and resume website"
 };
 
 export default function RootLayout({
